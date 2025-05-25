3a0b17de45291cd14d3464b36c20321a4e57afae
Allan Fernandes <Fernzallan14@gmail.com>
2025-05-25 18:37:52 +0100

    move

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@ echo "Copying build files to Nginx directory..."
 cp -r /tmp/codepipeline-artifacts/dist/* /var/www/html/
 echo "Restarting Nginx..."
 systemctl restart nginx
-echo "Deployment completed!scr"
+echo "Deployment completed!"
--- scripts/deploy.sh
+++
@@ -1,5 +0,0 @@
-echo "Copying build files to Nginx directory..."
-cp -r /tmp/codepipeline-artifacts/dist/* /var/www/html/
-echo "Restarting Nginx..."
-systemctl restart nginx
-echo "Deployment completed!"
